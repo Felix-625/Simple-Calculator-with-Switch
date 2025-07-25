a0736b6365166dc1fa5bebf407887e1bb1b3dc60
Q.M.S <Q.M.S@DESKTOP-AGG7OQP>
2025-07-25 07:46:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator_with_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1;
+            float num2;
+            string op; 
+
+            //Ask user yo input 1st num
+            Console.Write("Enter first num: ");
+            num1 = float.Parse(Console.ReadLine());
+
+            //Ask user to input 2nd num
+            Console.Write("Enter second num: ");
+            num2 = float.Parse(Console.ReadLine());
+
+            //Ask user to input operator(+,-,*,/)
+            Console.Write("Choose Operator(+, -, *, /): ");
+            op = Console.ReadLine();
+
+            switch (op)
+            {   // if user enter + it will add the two num
+                case "+":
+                    float sum = num1 + num2;
+                    Console.WriteLine("Sum is: " + sum);
+                    break;
+                // if user enter - it will subtract the two num
+                case "-":
+                    float diff = num1 - num2;
+                    Console.WriteLine("Difference is: " + diff);
+                    break;
+                // if user enter * it will multiply the two num
+                case "*":
+                    float prod = num1 * num2;
+                    Console.WriteLine("Product is: " + prod);
+                    break;
+                // if user enter / it will divide the two num
+                case "/":
+                    float quo = num1 - num2;
+                    // if the secnd num is 0 in will not divide
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Invalid second num must not be Zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Quotient is: " + quo);
+                    }
+                    break;
+
+
+            }
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Simple Calculator with Switch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Simple Calculator with Switch")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b0346d6f-1fc9-46e0-b92c-7ba08caa9ba5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
